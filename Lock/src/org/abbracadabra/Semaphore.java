9f02fb1b30e1c70ed09c620ffad07f241d5dcab5
abbracadabra <gyl_mail@qq.com>
2018-06-27 11:24:31 +0800

    Update Semaphore.java

--- Lock/src/org/abbracadabra/Semaphore.java
+++ Lock/src/org/abbracadabra/Semaphore.java
@@ -12,18 +12,45 @@ public class Semaphore {
 	}
 
 	public void acquire() {
-		atomicOps.decrementAndGet();
-		while (atomicOps.get()<1) {
-			//keep the thread dormant for a while to prevent the cpu from wasting too much time doing noting
+		while (true) {
+			long update = atomicOps.decrementAndGet();
+			if(update<0) {
+				atomicOps.incrementAndGet();
+			}else {
+				break;
+			}
+		}
+		
+	}
+	
+	public void acquire(long num) {
+		if(num<0) {
+			throw new RuntimeException("illegal argument");
+		}
+		while (true) {
+			long update = atomicOps.addAndGet(-num);
+			if(update<0) {
+				atomicOps.addAndGet(num);
+			}else {
+				break;
+			}
 			try {
 				Thread.currentThread().sleep(20);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+		
 	}
 
 	public void release() {
 		atomicOps.incrementAndGet();
+	}
+	
+	public void release(long num) {
+		if(num<0) {
+			throw new RuntimeException("illegal argument");
+		}
+		atomicOps.addAndGet(num);
 	}
 }
