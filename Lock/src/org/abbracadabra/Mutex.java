f5e049d624e4089d2cc81aa054786a30c2e4b4c7
abbracadabra <gyl_mail@qq.com>
2018-06-27 11:23:58 +0800

    Update Mutex.java

--- Lock/src/org/abbracadabra/Mutex.java
+++ Lock/src/org/abbracadabra/Mutex.java
@@ -8,7 +8,6 @@ public class Mutex {
 	public void Lock() {
 		while(!atomicOps.compareAndSet(true, false)) {
 			try {
-				//keep the thread dormant for a while to prevent the cpu from wasting too much time doing noting
 				Thread.currentThread().sleep(20);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
