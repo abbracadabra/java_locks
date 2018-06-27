b96f03e8c5c4b41b58189008bb981419a4f29c87
abbracadabra <gyl_mail@qq.com>
2018-06-27 11:25:20 +0800

    Add files via upload

---
+++ Lock/src/org/abbracadabra/CountDownLatch.java
@@ -0,0 +1,26 @@
+package org.abbracadabra;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CountDownLatch {
+
+		private AtomicLong atomicOps = new AtomicLong(0L);
+		
+		public CountDownLatch(long num) {
+			atomicOps.set(num);
+		}
+		
+		public void countDown() {
+			atomicOps.decrementAndGet();
+		}
+		
+		public void await() {
+			while (atomicOps.get()>0) {
+				try {
+					Thread.currentThread().sleep(20);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+}
